78aecf54d2e215984fa489e7a226f35dbe654319
Florian Henkes <florian.henkes@outlook.com>
2023-03-13 13:13:42 +0100

    updatet labels for tracking

--- cal_monitoring.py
+++ cal_monitoring.py
@@ -49,8 +49,8 @@ class monitoring(param.Parameterized):
                        "FEP Stability": plot_fep_stability_channels2d,
                       "Baseline": plot_bls, "Spectra": plot_energy_spectra}
     
-    plot_types_tracking_dict = {"Energy": plot_energy,"Energy Res Qbb": plot_energy_res_Qbb, 
-                                "Energy Res 2.6": plot_energy_res_2614, "A/E Mean": plot_aoe_mean,
+    plot_types_tracking_dict = {"Energy Calib. Const.": plot_energy,"FWHM Qbb": plot_energy_res_Qbb, 
+                                "FWHM FEP": plot_energy_res_2614, "A/E Mean": plot_aoe_mean,
                                 "A/E Sigma": plot_aoe_sig, "Tau": plot_tau,  "Alpha": plot_ctc_const}
     
     plot_type_tracking = param.ObjectSelector(default = list(plot_types_tracking_dict)[0], objects= list(plot_types_tracking_dict))
